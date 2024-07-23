f89c4da31b96b5a39f0c7bc0cb0c6afcfcd997a9
cleitonsilva14 <cleiton18ads@gmail.com>
2024-07-23 12:06:31 -0300

    Primeiro commit

---
+++ src/main/java/br/com/customloginlogoutpages/CustomloginlogoutpagesApplication.java
@@ -0,0 +1,13 @@
+package br.com.customloginlogoutpages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomloginlogoutpagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomloginlogoutpagesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/customloginlogoutpages/CustomloginlogoutpagesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.customloginlogoutpages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomloginlogoutpagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
